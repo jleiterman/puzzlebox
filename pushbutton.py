ad5a1aa2e22f6cbb6db23d2f2c5492d500065f3d
Justin Leiterman <Justin.Leiterman@gmail.com>
2020-03-27 15:03:49 +0000

    adding pushbutton demo

---
+++ pushbutton.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+def button_pressed(channel):
+    print("Button was pushed!")
+def button_released(channel):
+    print("Button was released!")
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(36,GPIO.RISING,callback=button_pressed, bouncetime=1000) # Setup event on pin 10 rising edge
+#GPIO.add_event_detect(36,GPIO.FALLING,callback=button_released, bouncetime=1000)
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
