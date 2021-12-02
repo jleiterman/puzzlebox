78fd49abf5c35b40a1454da3527ea0f53df43002
jleiterman <Justin.Leiterman@gmail.com>
2021-12-01 22:22:59 -0600

    adding failure recovery to system test

--- systemtest.py
+++ systemtest.py
@@ -15,39 +15,22 @@ from gpiozero import Button
 from signal import pause
 from time import time, sleep, localtime
 
+global debug_on
+debug_on = True
+
+#note 2021-11-29 found that there is a random failure in buttons working related to software.
+#to correct we can delete the button with `del button_[x]` and then recreate the button and 
+#then re run the button action to function mapping `button_[x].when_pressed = function`
+#can not find test to see that it is not working other than manual test of the physical switch
+#Idea to correct is to build a button push to exit each of the mapping steps.
+
+
 # setup input-output
 #Red LED display
 red_led = tm1637.TM1637(clk=17,dio=4)
 #White LCD screen
 serial = i2c(port=1, address=0x3C)
 lcd_screen = sh1106(serial)
-#buttons
-red_button = Button(26)
-yellow_button = Button(19)
-green_button = Button(13)
-blue_button = Button(6)
-black_button = Button(5)
-white_button = Button(11)
-#switches
-switch_1 = Button(14)
-switch_2 = Button(15)
-switch_3 = Button(18)
-switch_4 = Button(23)
-switch_5 = Button(24)
-switch_6 = Button(25)
-switch_7 = Button(8)
-switch_8 = Button(7)
-switch_9 = Button(12)
-#rotating buttons
-a_right = Button(27)
-a_left = Button(22)
-a_button = Button(10)
-b_right = Button(20)
-b_left = Button(16)
-b_button = Button(21)
-
-
-
 
 def lcd_text(text):
     with canvas(lcd_screen) as draw:
@@ -58,167 +41,119 @@ lcd_text("System Test\nresults displayed\nhere")
 
 ### test buttons ###
 
-def red_button_pressed():
-    print('Red button was pressed.')
-    lcd_text("Red button\nwas pressed")
-red_button.when_pressed = red_button_pressed
+def commission_test_repair_button(button_name,pin_number):
+    sleep(.5)
+    button = Button(pin_number)
+    def button_pressed():
+        print(button_name+' pressed')
+        lcd_text(button_name+'\nwas pressed')
+    button.when_pressed = button_pressed
+    print('press ' +button_name)
+    lcd_text('press ' +button_name)
+    while not button.wait_for_press(3):
+        del button
+        button = Button(pin_number)
+        button.when_pressed = button_pressed
+        print(button_name+' repaired')
+    return button
 
-def yellow_button_pressed():
-    print('Yellow button was pressed.')
-    lcd_text("Yellow button\nwas pressed")
-yellow_button.when_pressed = yellow_button_pressed
-
-def green_button_pressed():
-    print('Green button was pressed.')
-    lcd_text("Green button\nwas pressed")
-green_button.when_pressed = green_button_pressed
-
-def blue_button_pressed():
-    print('Blue button was pressed.')
-    lcd_text("Blue button\nwas pressed")
-blue_button.when_pressed = blue_button_pressed
-
-def black_button_pressed():
-    print('Black button was pressed.')
-    lcd_text("Black button\nwas pressed")
-black_button.when_pressed = black_button_pressed
-
-def white_button_pressed():
-    print('White button was pressed.')
-    lcd_text("White button\nwas pressed")
-white_button.when_pressed = white_button_pressed
-
-
+red_button = commission_test_repair_button('red button',26)
+yellow_button = commission_test_repair_button('yellow button',19)
+green_button = commission_test_repair_button('green button',13)
+blue_button = commission_test_repair_button('blue button',6)
+black_button = commission_test_repair_button('black button',5)
+white_button = commission_test_repair_button('white button',11)
 
 ###  TEST SWITCHES ###
 
-def switch_1_up():
-   print('switch 1 up')
-   lcd_text("Switch 1 up")
-switch_1.when_pressed = switch_1_up
+def commission_test_repair_switch(switch_name,pin_number):
+    sleep(.5)
+    button = Button(pin_number)
+    def switch_up():
+        print(switch_name+' up')
+        lcd_text(switch_name+' up')
+    button.when_pressed = switch_up
+    def switch_down():
+        print(switch_name+' down')
+        lcd_text(switch_name+' down')
+    button.when_released = switch_down
+    print('test ' +switch_name)
+    lcd_text('test ' +switch_name)
+    if button.wait_for_press(3):
+        if button.wait_for_release(3):
+            print("it works")
+            return button
+        else:
+            print("it failed release")
+            del button
+            return commission_test_repair_switch(switch_name,pin_number)
+    else:
+        print("it failed press")
+        del button
+        return commission_test_repair_switch(switch_name,pin_number)
 
-def switch_1_down():
-   print('switch 1 down')
-   lcd_text("Switch 1 down")
-switch_1.when_released = switch_1_down
-
-def switch_2_up():
-   print('switch 2 up')
-   lcd_text("Switch 2 up")
-switch_2.when_pressed = switch_2_up
-
-def switch_2_down():
-   print('switch 2 down')
-   lcd_text("Switch 2 down")
-switch_2.when_released = switch_2_down
-
-def switch_3_up():
-   print('switch 3 up')
-   lcd_text("Switch 3 up")
-switch_3.when_pressed = switch_3_up
-
-def switch_3_down():
-   print('switch 3 down')
-   lcd_text("Switch 3 down") 
-switch_3.when_released = switch_3_down
-
-def switch_4_up():
-   print('switch 4 up')
-   lcd_text("Switch 4 up")
-switch_4.when_pressed = switch_4_up
-
-def switch_4_down():
-   print('switch 4 down')
-   lcd_text("Switch 4 down")
-switch_4.when_released = switch_4_down
-def switch_5_up():
-   print('switch 5 up')
-   lcd_text("Switch 5 up")
-switch_5.when_pressed = switch_5_up
-
-def switch_5_down():
-   print('switch 5 down')
-   lcd_text("Switch 5 down")
-switch_5.when_released = switch_5_down
-
-def switch_6_up():
-   print('switch 6 up')
-   lcd_text("Switch 6 up")
-switch_6.when_pressed = switch_6_up
-
-def switch_6_down():
-   print('switch 6 down')
-   lcd_text("Switch 6 down")
-switch_6.when_released = switch_6_down
-
-def switch_7_up():
-   print('switch 7 up')
-   lcd_text("Switch 7 up")
-switch_7.when_pressed = switch_7_up
-
-def switch_7_down():
-   print('switch 7 down')
-   lcd_text("Switch 7 down")
-switch_7.when_released = switch_7_down
-
-def switch_8_up():
-   print('switch 8 up')
-   lcd_text("Switch 8 up")
-switch_8.when_pressed = switch_8_up
-
-def switch_8_down():
-   print('switch 8 down')
-   lcd_text("Switch 8 down")
-switch_8.when_released = switch_8_down
-
-def switch_9_up():
-   print('switch 9 up')
-   lcd_text("Switch 9 up")
-switch_9.when_pressed = switch_9_up
-
-def switch_9_down():
-   print('switch 9 down')
-   lcd_text("Switch 9 down")
-switch_9.when_released = switch_9_down
-
+switch_1 = commission_test_repair_switch('switch 1',14)
+switch_2 = commission_test_repair_switch('switch 2',15)
+switch_3 = commission_test_repair_switch('switch 3',18)
+switch_4 = commission_test_repair_switch('switch 4',23)
+switch_5 = commission_test_repair_switch('switch 5',24)
+switch_6 = commission_test_repair_switch('switch 6',25)
+switch_7 = commission_test_repair_switch('switch 7',8)
+switch_8 = commission_test_repair_switch('switch 8',7)
+switch_9 = commission_test_repair_switch('switch 9',12)
 
 ## Test Rotating Buttons ###
 
-#a rotating button
-def a_left_test():
-    if a_left.is_pressed:
-        print('a left')
-        lcd_text("A Left")
-a_right.when_pressed = a_left_test
 
-def a_right_test():
-    if a_right.is_pressed:
-        print('a right')
-        lcd_text("A Right")
-a_left.when_pressed = a_right_test
+def commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number):
+    sleep(.5)
+    right = Button(right_pin_number)
+    left = Button(left_pin_number)
+    button = Button(button_pin_number)
+    def left_test():
+        if left.is_pressed:
+            print(button_name+' left')
+            lcd_text(button_name+' left')
+    right.when_pressed = left_test
+    def right_test():
+        if right.is_pressed:
+            print(button_name+' right')
+            lcd_text(button_name+' right')
+    left.when_pressed = right_test
+    def button_pressed():
+        print(button_name+' pushed')
+        lcd_text(button_name+' pushed')
+    button.when_pressed = button_pressed
 
-def a_button_pressed():
-    print('a button push')
-    lcd_text("A pushed")
-a_button.when_pressed = a_button_pressed
+    print('test ' +button_name+' right left press')
+    lcd_text('test ' +button_name+'\nright left\n press')
+    if left.wait_for_press(3):
+        if right.is_pressed:
+            if right.wait_for_press(3):
+                if left.is_pressed:
+                    if button.wait_for_press(3):
+                        print("it works")
+                        return right, left, button;
+                    else:
+                        del right, left, button;
+                        return commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number) 
+                else:
+                    del right, left, button;
+                    return commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number) 
+            else:
+                del right, left, button;
+                return commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number) 
+        else:
+            del right, left, button;
+            return commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number)             
 
-#b rotating button
-def b_left_test():
-    if b_left.is_pressed:
-        print('b left')
-        lcd_text("B Left")
-b_right.when_pressed = b_left_test
+    else:
+        del right, left, button;
+        return commission_test_repair_roating_button(button_name,right_pin_number,left_pin_number,button_pin_number) 
 
-def b_right_test():
-    if b_right.is_pressed:
-        print('b right')
-        lcd_text("B Right")
-b_left.when_pressed = b_right_test
+a_right, a_left, a_button = commission_test_repair_roating_button('a',27,22,10)
+b_right, b_left, b_button = commission_test_repair_roating_button('b',20,16,21)
 
-def b_button_pressed():
-    print('b button push')
-    lcd_text("B pushed")
-b_button.when_pressed = b_button_pressed
 
 
 # start the countdown timer
@@ -235,4 +170,4 @@ def countdown(red_led,t):
     lcd_text("System Test\nComplete")
     red_led.numbers(0, 0, True)
 
-countdown(red_led,t=100)
+countdown(red_led,t=10)
